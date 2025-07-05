eb31297ab3b9d563660c7c89fd93d987f176e110
worshix <worshiplivingstone@gmail.com>
2025-07-05 16:21:09 +0200

    graphic design design is done

--- components/BlurryCirclesBackround.jsx
+++ components/BlurryCirclesBackround.jsx
@@ -1,13 +1,19 @@
 // components/BlurryCirclesBackground.tsx
 
 'use client'
+import { motion } from "framer-motion";
 
 export default function BlurryCirclesBackground() {
   return (
     <div className="absolute inset-0 overflow-hidden z-0">
-      <div className="absolute top-10 left-10 w-[400px] h-[400px] bg-orange-400 opacity-30 rounded-full blur-3xl animate-spinSlow" />
-      <div className="absolute bottom-20 right-10 w-[500px] h-[500px] bg-orange-300 opacity-25 rounded-full blur-3xl animate-float" />
-      <div className="absolute top-1/3 right-1/4 w-[300px] h-[300px] bg-orange-500 opacity-20 rounded-full blur-3xl animate-spinSlow" />
+      <motion.div className="absolute top-10 left-10 w-[400px] h-[400px] bg-orange-300 opacity-30 rounded-full blur-3xl animate-spinSlow" 
+        animate={{ x: 200, y: 200 }}
+        transition={{ duration: 8, repeat: Infinity, ease: "easeInOut", repeatType: "reverse" }}
+      />
+      <motion.div className="absolute bottom-20 right-10 w-[500px] h-[500px] bg-blue-300 opacity-25 rounded-full blur-3xl animate-float"
+        animate={{ x: -200, y: -200 }}
+        transition={{ duration: 8, repeat: Infinity, ease: "easeInOut", repeatType: "reverse" }}
+      />
     </div>
   );
 }
