ab53f0abdf3d66bd4486fd7077640b21ccdfd359
worshix <worshiplivingstone@gmail.com>
2025-07-08 22:14:09 +0200

    subscription working with pixel email

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,7 @@ export default function RootLayout({ children }) {
           toastOptions={{
             duration: 3000,
           }}
+          richColors
         />
       </body>
     </html>
